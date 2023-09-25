5a5f11e2fbeacba8f8f6c2bfcf2ecbe647b041f4
CyberMC-Tech <mathewconder3@gmail.com>
2023-09-25 03:14:27 -0400

    first commit

---
+++ lua/custom/configs/overrides.lua
@@ -0,0 +1,72 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "bash",
+    "dockerfile",
+    "python",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+    "bash-language-server",
+    "beautysh",
+    "docker-compose-language-service",
+    "dockerfile-language-server",
+    "shellfmt",
+    "shellcheck",
+    "python-lsp-server",
+    "black",
+    "pyright",
+    "pylint",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
